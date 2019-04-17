372632ca23ba348ddd3a80daf6df3268c590e3b2
Luka Pozega <luka.pozega@fer.hr>
2019-04-17 19:37:34 +0200

    init project

---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+
+#include "Config.h"
+
+int main(int argc, char** argv) {
+
+	std::cout << assembly_VERSION_MAJOR << "." << assembly_VERSION_MINOR << std::endl;
+
+	return 0;
+
+}
