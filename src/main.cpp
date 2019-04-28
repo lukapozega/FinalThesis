367c16bdf6c3fc3b30a40a6b3d78e002032252a1
Luka Pozega <luka.pozega@fer.hr>
2019-04-28 17:29:54 +0200

    Find path through graph

--- src/main.cpp
+++ src/main.cpp
@@ -32,30 +32,34 @@ class Vertex {
 public:
 	PAFObject read;
 	std::vector<Vertex*> vertices;
+	Vertex* parent = NULL;
 
 	Vertex(PAFObject read) : read(read) {
 		this->read = read;
 	}
 };
 
-Vertex create_graph(std::vector<std::unique_ptr<PAFObject>> &paf_objects) {
-	std::vector<Vertex> vertices;
+std::vector<Vertex*> create_graph(std::vector<Vertex> &vertices, std::vector<std::unique_ptr<PAFObject>> &paf_objects) {
 	for (auto const& paf: paf_objects) {
 		vertices.emplace_back(Vertex(*paf));
 	}
 	std::vector<Vertex>::iterator it = vertices.begin();
 	std::vector<Vertex>::iterator next;
+	std::vector<Vertex*> heads;
+	heads.emplace_back(&(*it));
 	while(it != --vertices.end()) {
 		next = std::next(it);
 		while ((*next).read.t_begin <= (*it).read.t_end) {
+			//std::cout << (*it).read.t_begin << std::endl;
 			it->vertices.emplace_back(&(*next));
 			next++;
 			if(next == vertices.end()) break;
 		}
+		if (next == std::next(it)) heads.emplace_back(&(*next));
 		it++;
 	}
 	FILE* file = fopen ("network.txt","w");
-	for (auto const& vertex: vertices) {
+	for (auto const& vertex : vertices) {
 		fprintf (file, "%d %d", vertex.read.t_begin, vertex.read.t_end);
 		for (auto const& next: vertex.vertices) {
 			fprintf (file, " %d", next->read.t_begin);
@@ -63,7 +67,32 @@ Vertex create_graph(std::vector<std::unique_ptr<PAFObject>> &paf_objects) {
 		fprintf (file, "\n");
 	}
 	fclose (file);
-	return vertices[0];
+	return heads;
+}
+
+Vertex* DepthFirstSearch(std::vector<Vertex*> heads) {
+	Vertex* max;
+	Vertex* head;
+	Vertex* longest;
+	int length=0;
+	int begin;
+	for (int i = 0; i < heads.size(); i++) {
+		head = heads[i];
+		begin = head->read.t_begin;
+		while(!head->vertices.empty()) {
+			max = head->vertices[0];
+			for (auto const& vertex : head->vertices) {
+				if (vertex->read.t_begin > max->read.t_begin) max = vertex;
+			}
+			max->parent = head;
+			head = max;
+		}
+		if (length < head->read.t_begin - begin) {
+			length = head->read.t_begin - begin;
+			longest = head;
+		}
+	}
+	return longest;
 }
 
 void sweepLineAlgorithm(std::vector<std::unique_ptr<PAFObject>> &paf_objects) {
@@ -84,6 +113,20 @@ void sweepLineAlgorithm(std::vector<std::unique_ptr<PAFObject>> &paf_objects) {
 		}
 		it = next;
 	}
+}
+
+void statistics(Vertex* end, FASTAEntity &reference) {
+	int count = 1;
+	FILE* file = fopen ("used_reads.txt","w");
+	int last_index = end->read.t_end;
+	while (end->parent != NULL) {
+		count++;
+		fprintf (file, "%s %d %d\n", end->read.query_name.c_str(), end->read.t_begin, end->read.t_end);
+		end = end->parent;
+	}
+	fclose (file);
+	printf("Genome coverage: %f%\n", (last_index - end->read.t_begin) / (float) reference.sequence.length() * 100);
+	printf("Number of used reads: %d\n", count);
 }
 
 int main(int argc, char** argv) {
@@ -107,8 +150,17 @@ int main(int argc, char** argv) {
 		printf("Genome can't be assembled\n");
 	}
 
-	create_graph(paf_objects);
+	std::vector<Vertex> vertices;
+	std::vector<Vertex*> heads = create_graph(vertices, paf_objects);
+
+	printf("Number of components: %lu\n", heads.size());
+
+	Vertex* end = DepthFirstSearch(heads);
+
+	statistics(end, reference);
 
 	return 0;
 
 }
+
+
